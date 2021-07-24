6673e5c5f28467970f6b2a6f6df27095a369e60a
Dan Tkachuk <daniil.tkachuk@gmail.com>
2021-07-25 00:00:52 +0300

    Arrow finish

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 'use strict';
 
+const arrow1 = document.querySelector('.arrow');
 const computerChoice = document.querySelector('#comp');
 
-computerChoice.style.display = 'none';
+
+
 computerChoice.style.backgroundImage = "url('/img/rock.jpg')";
+
+hideCompChoice(true);
+
+function hideCompChoice(yes) {
+  if (yes) {
+    computerChoice.style.display = 'none';
+  }
+}
+
+
